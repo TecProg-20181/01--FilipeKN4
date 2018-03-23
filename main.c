a4822ef04f41cb438db627543130f81542ddffbd
FilipeKN4 <filipekn4@gmail.com>
2018-03-23 18:21:45 -0500

    Translate variables and methods names into English

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ typedef struct image {
     unsigned int height;
 } Image;
 
-Image ler_imagem(Image img) {
+Image read_image(Image image) {
 
   // read type of image
   char p3[4];
@@ -20,19 +20,19 @@ Image ler_imagem(Image img) {
 
   // read width height and color of image
   int max_color;
-  scanf("%u %u %d", &img.width, &img.height, &max_color);
+  scanf("%u %u %d", &image.width, &image.height, &max_color);
 
   // read all pixels of image
-  for (unsigned int i = 0; i < img.height; ++i) {
-      for (unsigned int j = 0; j < img.width; ++j) {
-          scanf("%hu %hu %hu", &img.pixel[i][j].r,
-                               &img.pixel[i][j].g,
-                               &img.pixel[i][j].b);
+  for (unsigned int i = 0; i < image.height; ++i) {
+      for (unsigned int j = 0; j < image.width; ++j) {
+          scanf("%hu %hu %hu", &image.pixel[i][j].r,
+                               &image.pixel[i][j].g,
+                               &image.pixel[i][j].b);
 
       }
   }
 
-  return img;
+  return image;
 
 }
 
@@ -48,98 +48,98 @@ int max(int a, int b) {
   return b;
 }
 
-Pixel media_escala_de_cinza(Pixel pixel) {
-    unsigned short int soma = pixel.r + pixel.g + pixel.b;
+Pixel calculate_average_pixel(Pixel pixel) {
+    unsigned short int sum = pixel.r + pixel.g + pixel.b;
 
-    Pixel media = {};
-    media.r = soma/3;
-    media.g = soma/3;
-    media.b = soma/3;
+    Pixel average = {};
+    average.r = sum/3;
+    average.g = sum/3;
+    average.b = sum/3;
 
-    return media;
+    return average;
 }
 
-Image escala_de_cinza(Image img) {
+Image apply_grayscale_filter(Image image) {
 
-    for (unsigned int i = 0; i < img.height; ++i) {
-        for (unsigned int j = 0; j < img.width; ++j) {
-            Pixel media = media_escala_de_cinza(img.pixel[i][j]);
-            img.pixel[i][j] = media;
+    for (unsigned int i = 0; i < image.height; ++i) {
+        for (unsigned int j = 0; j < image.width; ++j) {
+            Pixel average = calculate_average_pixel(image.pixel[i][j]);
+            image.pixel[i][j] = average;
         }
     }
 
-    return img;
+    return image;
 }
 
-Pixel soma_blur(Pixel pixel, Pixel soma) {
+Pixel sum_blur_filter(Pixel pixel, Pixel sum) {
 
-  soma.r += pixel.r;
-  soma.g += pixel.g;
-  soma.b += pixel.b;
+  sum.r += pixel.r;
+  sum.g += pixel.g;
+  sum.b += pixel.b;
 
-  return soma;
+  return sum;
 }
 
-Pixel media_blur(Pixel media, int T) {
+Pixel calculate_average_area(Pixel average, int size) {
 
-  media.r /= T * T;
-  media.g /= T * T;
-  media.b /= T * T;
+  average.r /= size * size;
+  average.g /= size * size;
+  average.b /= size * size;
 
-  return media;
+  return average;
 }
 
-Image blur(Image img) {
-    int T = 0;
-    scanf("%d", &T);
+Image apply_blur_filter(Image image) {
+    int size = 0;
+    scanf("%d", &size);
 
-    for (unsigned short int i = 0; i < img.height; ++i) {
-        for (unsigned short int j = 0; j < img.width; ++j) {
-            Pixel soma = {0, 0, 0};
+    for (unsigned short int i = 0; i < image.height; ++i) {
+        for (unsigned short int j = 0; j < image.width; ++j) {
+            Pixel sum = {0, 0, 0};
 
-            int menor_height = min(img.height - 1, i + T/2);
-            int min_width = min(img.width - 1, j + T/2);
-            for(int x = max(0, i - T/2); x <= menor_height; ++x) {
-                for(int y = max(0, j - T/2); y <= min_width; ++y) {
-                    soma = soma_blur(img.pixel[x][y], soma);
+            int min_height = min(image.height - 1, i + size/2);
+            int min_width = min(image.width - 1, j + size/2);
+            for(int x = max(0, i - size/2); x <= min_height; ++x) {
+                for(int y = max(0, j - size/2); y <= min_width; ++y) {
+                    sum = sum_blur_filter(image.pixel[x][y], sum);
                 }
             }
-            img.pixel[i][j] = media_blur(soma, T);
+            image.pixel[i][j] = calculate_average_area(sum, size);
 
         }
     }
 
-    return img;
+    return image;
 }
 
-Image rotacionar90direita(Image img) {
-    Image rotacionada;
+Image rotate_right(Image image) {
+    Image rotated_image;
 
-    rotacionada.width = img.height;
-    rotacionada.height = img.width;
+    rotated_image.width = image.height;
+    rotated_image.height = image.width;
 
-    for (unsigned int i = 0, y = 0; i < rotacionada.height; ++i, ++y) {
-        for (int j = rotacionada.width - 1, x = 0; j >= 0; --j, ++x) {
-            rotacionada.pixel[i][j] = img.pixel[x][y];
+    for (unsigned int i = 0, y = 0; i < rotated_image.height; ++i, ++y) {
+        for (int j = rotated_image.width - 1, x = 0; j >= 0; --j, ++x) {
+            rotated_image.pixel[i][j] = image.pixel[x][y];
         }
     }
 
-    return rotacionada;
+    return rotated_image;
 }
 
-Image aplicar_rotacao(Image img) {
-    int quantas_vezes = 0;
-    scanf("%d", &quantas_vezes);
-    quantas_vezes %= 4;
+Image apply_rotation_filter(Image image) {
+    int rotation_number = 0;
+    scanf("%d", &rotation_number);
+    rotation_number %= 4;
 
-    for (int j = 0; j < quantas_vezes; ++j) {
-        img = rotacionar90direita(img);
+    for (int j = 0; j < rotation_number; ++j) {
+        image = rotate_right(image);
   }
 
-  return img;
+  return image;
 }
 
-Pixel subtrair_de_cor_max(Pixel pixel) {
+Pixel sub_max_color(Pixel pixel) {
     pixel.r = 255 - pixel.r;
     pixel.g = 255 - pixel.g;
     pixel.b = 255 - pixel.b;
@@ -147,96 +147,101 @@ Pixel subtrair_de_cor_max(Pixel pixel) {
   return pixel;
 }
 
-Image inverter_cores(Image img) {
-    for (unsigned int i = 0; i < img.height; ++i) {
-        for (unsigned int j = 0; j < img.width; ++j) {
-            img.pixel[i][j] = subtrair_de_cor_max(img.pixel[i][j]);
+Image apply_color_invert_filter(Image image) {
+    for (unsigned int i = 0; i < image.height; ++i) {
+        for (unsigned int j = 0; j < image.width; ++j) {
+            image.pixel[i][j] = sub_max_color(image.pixel[i][j]);
         }
     }
-    return img;
+    return image;
 }
 
-Image cortar_imagem(Image img) {
-    Image cortada;
+Image apply_image_cut_filter(Image image) {
+    Image cropped_image;
 
     int x, y;
     scanf("%d %d", &x, &y);
     int width, height;
     scanf("%d %d", &width, &height);
 
-    cortada.width = width;
-    cortada.height = height;
+    cropped_image.width = width;
+    cropped_image.height = height;
 
     for(int i = 0; i < height; ++i) {
         for(int j = 0; j < width; ++j) {
-            cortada.pixel[i][j] = img.pixel[i + y][j + x];
+            cropped_image.pixel[i][j] = image.pixel[i + y][j + x];
         }
     }
 
-    return cortada;
+    return cropped_image;
 }
 
-int calcular_menor_r(double a, double b, double c, Pixel pixel) {
+int calculate_min_value(double a, double b, double c, Pixel pixel) {
   int p =  pixel.r * a + pixel.g * b + pixel.b * c;
-  //int menor_r = (255 >  p) ? p : 255;
-  int menor_r = min(255, p);
+  int min_value = min(255, p);
 
-  return menor_r;
+  return min_value;
 }
 
-Image filtro_sepia(Image img) {
-  for (unsigned int x = 0; x < img.height; ++x) {
-      for (unsigned int j = 0; j < img.width; ++j) {
+Image apply_sepia_filter(Image image) {
+  for (unsigned int x = 0; x < image.height; ++x) {
+      for (unsigned int j = 0; j < image.width; ++j) {
           Pixel pixel;
-          pixel = img.pixel[x][j];
+          pixel = image.pixel[x][j];
 
-          img.pixel[x][j].r = calcular_menor_r(0.393, 0.769, 0.189, pixel);
-          img.pixel[x][j].g = calcular_menor_r(0.349, 0.686, 0.168, pixel);
-          img.pixel[x][j].b = calcular_menor_r(0.272, 0.534, 0.131, pixel);
+          image.pixel[x][j].r = calculate_min_value(0.393, 0.769, 0.189, pixel);
+          image.pixel[x][j].g = calculate_min_value(0.349, 0.686, 0.168, pixel);
+          image.pixel[x][j].b = calculate_min_value(0.272, 0.534, 0.131, pixel);
       }
   }
 
-  return img;
+  return image;
 }
 
-Image espelhamento_vertical(Image img) {
+Image apply_vertical_mirroring_filter(Image image) {
   int horizontal = 0;
   scanf("%d", &horizontal);
 
-  int width = img.width, height = img.height;
+  int width = image.width, height = image.height;
 
-  if (horizontal == 1) width /= 2;
-  else height /= 2;
+  if (horizontal == 1) {
+      width /= 2;
+  } else {
+      height /= 2;
+  }
 
-  for (int i2 = 0; i2 < height; ++i2) {
+  for (int i = 0; i < height; ++i) {
       for (int j = 0; j < width; ++j) {
-          int x = i2, y = j;
+          int x = i, y = j;
 
-          if (horizontal == 1) y = img.width - 1 - j;
-          else x = img.height - 1 - i2;
+          if (horizontal == 1) {
+              y = image.width - 1 - j;
+          } else {
+              x = image.height - 1 - i;
+          }
 
           Pixel aux1;
-          aux1 = img.pixel[i2][j];
-          img.pixel[i2][j] = img.pixel[x][y];
-          img.pixel[x][y] = aux1;
+          aux1 = image.pixel[i][j];
+          image.pixel[i][j] = image.pixel[x][y];
+          image.pixel[x][y] = aux1;
       }
   }
 
-  return img;
+  return image;
 }
 
-void mostrar_imagem(Image img) {
+void show_image(Image image) {
     // print type of image
     printf("P3\n");
     // print width height and color of image
-    printf("%u %u\n255\n", img.width, img.height);
+    printf("%u %u\n255\n", image.width, image.height);
 
     // print pixels of image
-    for (unsigned int i = 0; i < img.height; ++i) {
-        for (unsigned int j = 0; j < img.width; ++j) {
-            printf("%hu %hu %hu ", img.pixel[i][j].r,
-                                   img.pixel[i][j].g,
-                                   img.pixel[i][j].b);
+    for (unsigned int i = 0; i < image.height; ++i) {
+        for (unsigned int j = 0; j < image.width; ++j) {
+            printf("%hu %hu %hu ", image.pixel[i][j].r,
+                                   image.pixel[i][j].g,
+                                   image.pixel[i][j].b);
 
         }
         printf("\n");
@@ -244,50 +249,50 @@ void mostrar_imagem(Image img) {
 }
 
 int main() {
-    Image img;
+    Image image;
 
-    img = ler_imagem(img);
+    image = read_image(image);
 
-    int n_opcoes;
-    scanf("%d", &n_opcoes);
+    int options;
+    scanf("%d", &options);
 
-    for(int i = 0; i < n_opcoes; ++i) {
-        int opcao;
-        scanf("%d", &opcao);
+    for(int i = 0; i < options; ++i) {
+        int option;
+        scanf("%d", &option);
 
-        switch(opcao) {
-            case 1: { // Escala de Cinza
-                img = escala_de_cinza(img);
+        switch(option) {
+            case 1: { // Grayscale filter
+                image = apply_grayscale_filter(image);
                 break;
             }
-            case 2: { // Filtro Sepia
-                img = filtro_sepia(img);
+            case 2: { // Sepia filter
+                image = apply_sepia_filter(image);
                 break;
             }
-            case 3: { // Blur
-                img = blur(img);
+            case 3: { // Blur filter
+                image = apply_blur_filter(image);
                 break;
             }
-            case 4: { // Rotacao
-                img = aplicar_rotacao(img);
+            case 4: { // Rotation filter
+                image = apply_rotation_filter(image);
                 break;
             }
-            case 5: { // Espelhamento
-                img = espelhamento_vertical(img);
+            case 5: { // Vertical mirrorin filter
+                image = apply_vertical_mirroring_filter(image);
                 break;
             }
-            case 6: { // Inversao de Cores
-                img = inverter_cores(img);
+            case 6: { // Color invert filter
+                image = apply_color_invert_filter(image);
                 break;
             }
-            case 7: { // Cortar Imagem
-                img = cortar_imagem(img);
+            case 7: { // Image cut filter
+                image = apply_image_cut_filter(image);
                 break;
             }
         }
 
     }
-    mostrar_imagem(img);
+    show_image(image);
 
     return 0;
 }
